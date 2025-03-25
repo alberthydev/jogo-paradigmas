c6480bb033d9fd724327a966c6df4096023df017
Albert <hygor.marques@protonmail.com>
2025-03-25 08:14:46 -0300

    Sprites adicionado

---
+++ sprites.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void print_fight_1(){
+    printf("; VIDA: S2 S2 S2                                               \n");
+    printf("; OPONENTE: S2 S2 S2                                           \n");
+    printf(";                                         mmmmmmmmmmm          \n");
+    printf(";                                        mmmmmmmmmmmmmm        \n");
+    printf(";                                      mmmmmmmmmmmmmmmmmmm     \n");
+    printf(";                                      mmmmmmmmmmmmmmmmmm      \n");
+    printf(";                                     mmmm       mmmmmmmm      \n");
+    printf(";                                    mmx ----    ----   m      \n");
+    printf(";                                     x  +++--  --+++   x      \n");
+    printf(";                                     x   ++  t  +++    x      \n");
+    printf(";                                     xx     tt        xx      \n");
+    printf(";                                      xx   tttt      xqqqqq   \n");
+    printf(";                               qqqqqqq xx   hhhhhh qqqqqq qq  \n");
+    printf(";                             qq      qq xx   hhhh qqxx   qq q \n");
+    printf(";                            qqq       qq xxx     xqq      qqq \n");
+    printf(";                            qqq        q  xxxxxxxxqq      qqq \n");
+    printf(";                             qqq      qqq         qqqqqqqqqq  \n");
+    printf(";                              qqqqqqqqq qq           qq    qq \n");
+    printf(";                                qqq      qq           qq    qq\n");
+    printf(";                                  q       q                   \n");
+    printf(";                                                              \n");
+    printf(";                                                              \n");
+    printf(";     xxxxxxxxx              xxxxxxxxxx                        \n");
+    printf(";   xxx        xx          xxx       xxxxx                     \n");
+    printf(";   xx           x        xxx           xxx                    \n");
+    printf(";   xx            x        xx             xx                   \n");
+    printf(";   x          xxxx         xxx           xx                   \n");
+    printf(";   x         xx  x        xx  x          xx                   \n");
+    printf(";   x            xx        x   xx        xx                    \n");
+    printf(";   x          x          xx   x       xxx                     \n");
+    printf(";   xxx      xxx           xx      xxxxx                       \n");
+}
