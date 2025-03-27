27e3e4787edf2d6ee5ead331c7f30269f0da3b84
Albert <hygor.marques@protonmail.com>
2025-03-26 23:54:02 -0300

    Sprites adicionados, iniciado funcoes do teclado

--- main.c
+++ main.c
@@ -1,14 +1,52 @@
 #include <curses.h>
 #include <unistd.h>
+#include "sprites.c"
 
 void main(){
     WINDOW* win = initscr();
     keypad(win, true);
     nodelay(win, true);
+    curs_set(0);
+
+    int playerH = 5;
+    int opponentH = 5;
+    int sprite = 1;
+    int height = 32;
+    int width = 62;
+
+    draw_box();
+
+    draw_text_centered("BOXERS", 10, width);
+    draw_text_centered("1 - INICIAR", 14, width);
+    draw_text_centered("2 - COMO JOGAR", 16, width);
+    draw_text_centered("3 - SAIR", 18, width);
+
+    refresh();  // Atualiza a tela
+
     while (true) {
         int pressed = wgetch(win);
-        if(pressed == KEY_LEFT){
-            printw("Left key pressed\n");
+        if(pressed == '1'){
+            //printw("Left key pressed\n");
+            for(;;){
+                clear();  
+
+                healthBar(playerH, opponentH);
+                refresh();
+                if(sprite == 1){
+                    fight_sprite_1();
+                    refresh();
+                    sprite = 0;
+                }
+                else{
+                    fight_sprite_2();
+                    refresh();  
+                    sprite = 1;
+                }
+                usleep(450000);  
+            }
+        }else if(pressed == '3'){
+            //printw("Right key pressed\n");
+            break;
         }
         usleep(100000);
     }
--- sprites.c
+++ sprites.c
@@ -1,35 +1,127 @@
-#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <ncurses.h>
 
-void print_fight_1(){
-    printf("; VIDA: S2 S2 S2                                               \n");
-    printf("; OPONENTE: S2 S2 S2                                           \n");
-    printf(";                                         mmmmmmmmmmm          \n");
-    printf(";                                        mmmmmmmmmmmmmm        \n");
-    printf(";                                      mmmmmmmmmmmmmmmmmmm     \n");
-    printf(";                                      mmmmmmmmmmmmmmmmmm      \n");
-    printf(";                                     mmmm       mmmmmmmm      \n");
-    printf(";                                    mmx ----    ----   m      \n");
-    printf(";                                     x  +++--  --+++   x      \n");
-    printf(";                                     x   ++  t  +++    x      \n");
-    printf(";                                     xx     tt        xx      \n");
-    printf(";                                      xx   tttt      xqqqqq   \n");
-    printf(";                               qqqqqqq xx   hhhhhh qqqqqq qq  \n");
-    printf(";                             qq      qq xx   hhhh qqxx   qq q \n");
-    printf(";                            qqq       qq xxx     xqq      qqq \n");
-    printf(";                            qqq        q  xxxxxxxxqq      qqq \n");
-    printf(";                             qqq      qqq         qqqqqqqqqq  \n");
-    printf(";                              qqqqqqqqq qq           qq    qq \n");
-    printf(";                                qqq      qq           qq    qq\n");
-    printf(";                                  q       q                   \n");
-    printf(";                                                              \n");
-    printf(";                                                              \n");
-    printf(";     xxxxxxxxx              xxxxxxxxxx                        \n");
-    printf(";   xxx        xx          xxx       xxxxx                     \n");
-    printf(";   xx           x        xxx           xxx                    \n");
-    printf(";  xxx            x        xx             xx                   \n");
-    printf(";  xx          xxxx         xxx           xx                   \n");
-    printf(";  xx         xx  x        xx  x          xx                   \n");
-    printf(";  xx            xx        x   xx        xx                    \n");
-    printf(";  xx          x          xx   x       xxx                     \n");
-    printf(";   xxx      xxx           xx      xxxxx                       \n");
+void healthBar(int, int);
+void print_fight_1();
+
+// CANVAS 32x62
+
+void draw_box() {
+    int height = 32;
+    int width = 62;
+    int start_x = 0;
+    int start_y = 0;
+
+    // Desenha a borda da caixa usando strings
+    for (int i = 0; i < width; i++) {
+        mvaddch(start_y, start_x + i, '-');  // Linha superior
+        mvaddch(start_y + height - 1, start_x + i, '-');  // Linha inferior
+    }
+    for (int i = 0; i < height; i++) {
+        mvaddch(start_y + i, start_x, '|');  // Linha esquerda
+        mvaddch(start_y + i, start_x + width - 1, '|');  // Linha direita
+    }
+    mvaddch(start_y, start_x, '+');  // Canto superior esquerdo
+    mvaddch(start_y, start_x + width - 1, '+');  // Canto superior direito
+    mvaddch(start_y + height - 1, start_x, '+');  // Canto inferior esquerdo
+    mvaddch(start_y + height - 1, start_x + width - 1, '+');  // Canto inferior direito
 }
+
+void draw_text_centered(const char* text, int y, int width) {
+    int text_len = strlen(text);
+    int start_x = (width - text_len) / 2;
+    mvprintw(y, start_x, "%s", text);
+}
+
+void healthBar(int playerLife, int opponentLife){
+    // Desenha a barra de vida do jogador
+    printw("; PLAYER: ");
+    for (int i = 0; i < playerLife; i++) {
+        printw("S2 ");  // Exibe o coração
+    }
+    for (int i = playerLife; i < 3; i++) {
+        printw("  ");  // Espaço em branco para representar a vida perdida
+    }
+    printw("\n");
+
+    // Desenha a barra de vida do oponente
+    printw("; OPONENTE: ");
+    for (int i = 0; i < opponentLife; i++) {
+        printw("S2 ");  // Exibe o coração
+    }
+    for (int i = opponentLife; i < 3; i++) {
+        printw("  ");  // Espaço em branco para representar a vida perdida
+    }
+    printw("\n");
+}
+
+void fight_sprite_1(){
+    printw(";                                                              \n");
+    printw(";                                         mmmmmmmmmmm          \n");
+    printw(";                                        mmmmmmmmmmmmmm        \n");
+    printw(";                                      mmmmmmmmmmmmmmmmmmm     \n");
+    printw(";                                      mmmmmmmmmmmmmmmmmm      \n");
+    printw(";                                     mmmm       mmmmmmmm      \n");
+    printw(";                                    mmx ----    ----   m      \n");
+    printw(";                                     x  +++--  --+++   x      \n");
+    printw(";                                     x   ++  t  +++    x      \n");
+    printw(";                                     xx     tt        xx      \n");
+    printw(";                                      xx   tttt      xqqqqq   \n");
+    printw(";                               qqqqqqq xx   hhhhhh qqqqqq qq  \n");
+    printw(";                             qq      qq xx   hhhh qqxx   qq q \n");
+    printw(";                            qqq       qq xxx     xqq      qqq \n");
+    printw(";                            qqq        q  xxxxxxxxqq      qqq \n");
+    printw(";                             qqq      qqq         qqqqqqqqqq  \n");
+    printw(";                              qqqqqqqqq qq           qq    qq \n");
+    printw(";                                qqq      qq           qq    qq\n");
+    printw(";                                                              \n");
+    printw(";                                                              \n");
+    printw(";     xxxxxxxxx              xxxxxxxxxx                        \n");
+    printw(";   xxx        xx          xxx       xxxxx                     \n");
+    printw(";   xx           x        xxx           xxx                    \n");
+    printw(";  xxx            x        xx             xx                   \n");
+    printw(";  xx          xxxx         xxx           xx                   \n");
+    printw(";  xx         xx  x        xx  x          xx                   \n");
+    printw(";  xx            xx        x   xx        xx                    \n");
+    printw(";  xx          x          xx   x       xxx                     \n");
+    printw(";   xxx      xxx           xx      xxxxx                       \n");
+    printw(";    x       x             x        x                          \n");
+}
+
+void fight_sprite_2(){
+    printw(";                                                              \n");
+    printw(";                                 mmmmmmmmmmm                  \n");
+    printw(";                                mmmmmmmmmmmmmm                \n");
+    printw(";                              mmmmmmmmmmmmmmmmmmm             \n");
+    printw(";                              mmmmmmmmmmmmmmmmmm              \n");
+    printw(";                             mmmm       mmmmmmmm              \n");
+    printw(";                            mmx ----    ----   m              \n");
+    printw(";                             x  +++--  --+++   x              \n");
+    printw(";                             x   ++  t  +++    x              \n");
+    printw(";                             xx     tt        xx              \n");
+    printw(";                              xx   tttt      xqqqqq           \n");
+    printw(";                       qqqqqqq xx   hhhhhh qqqqqq qq          \n");
+    printw(";                     qq      qq xx   hhhh qqxx   qq q         \n");
+    printw(";                    qqq       qq xxx     xqq      qqq         \n");
+    printw(";                    qqq        q  xxxxxxxxqq      qqq         \n");
+    printw(";                     qqq      qqq         qqqqqqqqqq          \n");
+    printw(";                      qqqqqqqqq qq           qq    qq         \n");
+    printw(";                        qqq      qq           qq    qq        \n");
+    printw(";                                                              \n");
+    printw(";                                                              \n");
+    printw(";      xxxxxxxxx              xxxxxxxxxx                        \n");
+    printw(";    xxx        xx          xxx       xxxxx                     \n");
+    printw(";    xx           x        xxx           xxx                    \n");
+    printw(";   xxx            x        xx             xx                   \n");
+    printw(";   xx          xxxx         xxx           xx                   \n");
+    printw(";   xx         xx  x        xx  x          xx                   \n");
+    printw(";   xx            xx        x   xx        xx                    \n");
+    printw(";   xx          x          xx   x       xxx                     \n");
+    printw(";    xxx      xxx           xx      xxxxx                       \n");
+    printw(";     x       x             x        x                          \n");
+}
+
+//void punch(){
+//    
+//}
